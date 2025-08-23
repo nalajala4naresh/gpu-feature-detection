19ec8a86bebe7f161a99a90f10c5d029dbeede1f
Ubuntu <ubuntu@ip-172-31-43-118.ec2.internal>
2025-08-23 21:10:39 +0000

    fix: just  using chromium project

--- playwright.config.js
+++ playwright.config.js
@@ -28,13 +28,5 @@ export default defineConfig({
         },
       },
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
   ],
 })
